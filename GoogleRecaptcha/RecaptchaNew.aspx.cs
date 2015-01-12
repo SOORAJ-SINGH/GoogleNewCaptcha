118529097486a168ddba67921fa90ca9eca16b66
Sooraj Singh <sooraj12002@gmail.com>
2015-01-12 11:36:22 +0530

    google recaptcha completed

--- GoogleRecaptcha/RecaptchaNew.aspx.cs
+++ GoogleRecaptcha/RecaptchaNew.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Net;
 
 namespace GoogleRecaptcha
 {
@@ -16,7 +19,75 @@ namespace GoogleRecaptcha
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
+            //validate Recaptcha
+            if (Validate())
+            {
+
+
+
+                lblMsg.Text = "Valid Recaptcha";
+                lblMsg.ForeColor = System.Drawing.Color.Green;
+
+
+            }
+
+            else
+            {
+
+                lblMsg.Text = "Not Valid Recaptcha";
+                lblMsg.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        public bool Validate()
+        {
+
+            //Getting Response String Appned to Post Method
+            string Response = Request["g-recaptcha-response"];
+
+            bool Valid = false;
+            //Request to Google Server
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(" https://www.google.com/recaptcha/api/siteverify?secret=6LeDTAATAAAAAD8e7bH0ygZhyVPINSwO_8q04_K7&response=" + Response);
+
+            try
+            {
+                //Google recaptcha Responce 
+                using (WebResponse wResponse = req.GetResponse())
+                {
+
+                    using (StreamReader readStream = new StreamReader(wResponse.GetResponseStream()))
+                    {
+                        string jsonResponse = readStream.ReadToEnd();
+
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        MyObject data = js.Deserialize<MyObject>(jsonResponse);// Deserialize Json 
+
+                        Valid = Convert.ToBoolean(data.success);
+
+
+                    }
+                }
+
+                return Valid;
+
+            }
+            catch (WebException ex)
+            {
+                throw ex;
+            }
+
 
         }
+
+
+
+
     }
+}
+
+public class MyObject
+{
+    public string success { get; set; }
+
+
 }
